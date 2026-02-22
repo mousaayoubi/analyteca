495b2063013d774521ce03f678bb23a37cc1f1ec
Mousa Ayoubi <mousa@mousaayoubi.com>
2026-02-22 17:37:22 +0000

    Added metrics  routes

---
+++ apps/api/src/modules/metrics/metrics.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { requireAuh } from "../../middleware/auth.js";
+import { getSummary } from "./metrics.service.js";
+
+const router = Router();
+
+router.get("/summary", requireAuth, async (req, res) => {
+
+	const { from, to } = req.query;
+
+	const data = await getSummary({ from, to });
+	res.json(data);
+});
+
+export default router;
